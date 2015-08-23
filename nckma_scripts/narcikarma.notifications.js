b284d7a61fbdc6194047ec6aad6191f8a4100671
Bryn Mosher <sites@brynmosher.com>
2015-08-23 03:35:38 -0700

    Added errors to notifications

--- nckma_scripts/narcikarma.notifications.js
+++ nckma_scripts/narcikarma.notifications.js
@@ -14,46 +14,59 @@
 	var presets = {
 		'error': {
 			'type': 'basic',
-			'title': 'Note',
+			'title': 'ERROR!!!',
 			'message': 'Narcikarma unknown error!!!',
-			'iconUrl': '../nckma_assets/img/iconWarn.png',
+			'iconUrl': '../nckma_assets/img/iconErr64.png',
 			'isClickable': true,
+			'priority': 2,
 			'contextMessage': 'Narcikarma '+nckma.version().str
 		},
 		'note': {
 			'type': 'basic',
 			'title': 'Note',
 			'message': 'Narcikarma unknown notification!',
-			'iconUrl': '../nckma_assets/img/icon.png',
+			'iconUrl': '../nckma_assets/img/icon64.png',
 			'isClickable': true,
+			'priority': 0,
 			'contextMessage': 'Narcikarma '+nckma.version().str
 		},
 		'warning': {
 			'type': 'basic',
 			'title': 'Warning!',
 			'message': 'Narcikarma unknown warning!',
-			'iconUrl': '../nckma_assets/img/iconWarn.png',
+			'iconUrl': '../nckma_assets/img/iconWarn64.png',
 			'isClickable': true,
+			'priority': 1,
 			'contextMessage': 'Narcikarma '+nckma.version().str
 		}
 	};
 	var titles = {
+		'note': 'Info',
+		'error': 'ERROR!!!',
 		'warning': 'Warning!',
 	};
 	var waitNote = 5000;
+	var waitError = 0;
 	var waitWarning = 10000;
 
 	function handle_clicked (noteId) {
 		chrome.notifications.clear(noteId);
 	}
 
-	function notify (config, cb) {
+	function notify (config, wait) {
 		var id = bpmv.ego();
+		var cb = parseInt(wait, 10) < 1 ? null : function() {
+			setTimeout(function() {
+				try {
+					chrome.notifications.clear(id);
+				} catch(e) {}
+			}, wait);
+		};
 
 		chrome.notifications.create(id, config, cb);
 	}
 
-	function notify_group (arr, config, cb) {
+	function notify_group (arr, config, wait) {
 		var iter;
 		var len;
 		var items = [];
@@ -83,34 +96,40 @@
 			config.items = items;
 			config.type = 'list';
 
-			notify(config, cb);
+			notify(config, wait);
 		}
 	}
+
+	nckma.notify.err = function(error) {
+		var cfg = $.extend({}, presets['error']);
+
+		cfg.message = ''+error;
+
+		notify(cfg, waitError);
+		nckma.track('func', 'notify.error', 'nkExec');
+	};
+
+	nckma.notify.err_group = function (errArr) {
+		var cfg = $.extend({}, presets['error']);
+
+		notify_group(errArr, cfg, waitError);
+		nckma.track('func', 'notify.err_group', 'nkExec');
+	};
 
 	nckma.notify.note = function(note) {
 		var cfg = $.extend({}, presets['note']);
 
 		cfg.message = ''+note;
 
-		notify(cfg, function() {
-			setTimeout(function() {
-				chrome.notifications.clear(id);
-			}, waitNote);
-		});
+		notify(cfg, waitNote);
 		nckma.track('func', 'notify.note', 'nkExec');
 	};
 
-	nckma.notify.warn_group = function (warnArr) {
-		var cfg = $.extend({}, presets['warning']);
+	nckma.notify.note_group = function (noteArr) {
+		var cfg = $.extend({}, presets['note']);
 
-		cfg.title = titles.warning;
-
-		notify_group(warnArr, cfg, function() {
-			setTimeout(function() {
-				chrome.notifications.clear(id);
-			}, waitWarning);
-		});
-		nckma.track('func', 'notify.warn_group', 'nkExec');
+		notify_group(noteArr, cfg, waitNote);
+		nckma.track('func', 'notify.note_group', 'nkExec');
 	};
 
 	nckma.notify.warn = function(warning) {
@@ -118,12 +137,15 @@
 
 		cfg.message = ''+warning;
 
-		notify(cfg, function() {
-			setTimeout(function() {
-				chrome.notifications.clear(id);
-			}, waitWarning);
-		});
+		notify(cfg, waitWarning);
 		nckma.track('func', 'notify.warn', 'nkExec');
+	};
+
+	nckma.notify.warn_group = function (warnArr) {
+		var cfg = $.extend({}, presets['warning']);
+
+		notify_group(warnArr, cfg, waitWarning);
+		nckma.track('func', 'notify.warn_group', 'nkExec');
 	};
 
 	chrome.notifications.onClicked.addListener(handle_clicked)
