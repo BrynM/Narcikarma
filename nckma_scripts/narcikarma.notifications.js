ec96b199acfe8684897d5178dcbf97872a19ba8f
Bryn Mosher <sites@brynmosher.com>
2015-08-23 03:08:53 -0700

    notifications fleshed out

--- nckma_scripts/narcikarma.notifications.js
+++ nckma_scripts/narcikarma.notifications.js
@@ -1,34 +1,131 @@
-// https://developer.chrome.com/apps/notifications#type-NotificationOptions
-var notification = chrome.notifications.create('abcde', {
-		type: 'basic',
-		// icon url - can be relative
-		iconUrl: '../nckma_assets/img/icon.png',
-  		// notification title
-  		title: 'Narcikamra v'+nckma.version().str+' startup!',
-  		// notification body text
-  		message: 'Testing those notifications...',
-  		buttons: [
-  			{
-  				title: 'close'
-  			}
-		]
-	},
-	function(notId) {
-		console.log('notified', arguments);
-		setTimeout(function() {
-			chrome.notifications.clear(notId, function() {
-				console.log('notify closed', arguments);
-			});
-		}, 5000);
+(function () {
+
+	/*
+	********************************************************************************
+	********************************************************************************
+	* Support for UI pages
+	* https://developer.chrome.com/apps/notifications#type-NotificationOptions
+	********************************************************************************
+	********************************************************************************
+	*/
+
+	nckma.notify = {};
+
+	var presets = {
+		'error': {
+			'type': 'basic',
+			'title': 'Note',
+			'message': 'Narcikarma unknown error!!!',
+			'iconUrl': '../nckma_assets/img/iconWarn.png',
+			'isClickable': true,
+			'contextMessage': 'Narcikarma '+nckma.version().str
+		},
+		'note': {
+			'type': 'basic',
+			'title': 'Note',
+			'message': 'Narcikarma unknown notification!',
+			'iconUrl': '../nckma_assets/img/icon.png',
+			'isClickable': true,
+			'contextMessage': 'Narcikarma '+nckma.version().str
+		},
+		'warning': {
+			'type': 'basic',
+			'title': 'Warning!',
+			'message': 'Narcikarma unknown warning!',
+			'iconUrl': '../nckma_assets/img/iconWarn.png',
+			'isClickable': true,
+			'contextMessage': 'Narcikarma '+nckma.version().str
+		}
+	};
+	var titles = {
+		'warning': 'Warning!',
+	};
+	var waitNote = 5000;
+	var waitWarning = 10000;
+
+	function handle_clicked (noteId) {
+		chrome.notifications.clear(noteId);
 	}
-);
 
-/*
-// Or create an HTML notification:
-var notification = webkitNotifications.createHTMLNotification(
-  '../nckma_html/notification.html'  // html url - can be relative
-);
+	function notify (config, cb) {
+		var id = bpmv.ego();
 
-// Then show the notification.
-notification.show();
-*/
+		chrome.notifications.create(id, config, cb);
+	}
+
+	function notify_group (arr, config, cb) {
+		var iter;
+		var len;
+		var items = [];
+		var id;
+
+		if(bpmv.str(arr)) {
+			config.message = arr;
+			return notify(config);
+		}
+
+		if(!bpmv.arr(arr)) {
+			return;
+		}
+
+		len = arr.length;
+
+		for(iter = 0; iter < len; iter++) {
+			if(bpmv.str(arr[iter])) {
+				items.push({
+					'title': arr[iter],
+					'message': ''
+				});
+			}
+		}
+
+		if(bpmv.arr(items)) {
+			config.items = items;
+			config.type = 'list';
+
+			notify(config, cb);
+		}
+	}
+
+	nckma.notify.note = function(note) {
+		var cfg = $.extend({}, presets['note']);
+
+		cfg.message = ''+note;
+
+		notify(cfg, function() {
+			setTimeout(function() {
+				chrome.notifications.clear(id);
+			}, waitNote);
+		});
+		nckma.track('func', 'notify.note', 'nkExec');
+	};
+
+	nckma.notify.warn_group = function (warnArr) {
+		var cfg = $.extend({}, presets['warning']);
+
+		cfg.title = titles.warning;
+
+		notify_group(warnArr, cfg, function() {
+			setTimeout(function() {
+				chrome.notifications.clear(id);
+			}, waitWarning);
+		});
+		nckma.track('func', 'notify.warn_group', 'nkExec');
+	};
+
+	nckma.notify.warn = function(warning) {
+		var cfg = $.extend({}, presets['warning']);
+
+		cfg.message = ''+warning;
+
+		notify(cfg, function() {
+			setTimeout(function() {
+				chrome.notifications.clear(id);
+			}, waitWarning);
+		});
+		nckma.track('func', 'notify.warn', 'nkExec');
+	};
+
+	chrome.notifications.onClicked.addListener(handle_clicked)
+})();
+
